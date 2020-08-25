db26e173e0193fa711a916a2a4b95ad3531c4557
songxuejun <sxjworld@126.com>
2020-08-25 16:27:30 +0800

    初始化项目提交

---
+++ src/main/java/com/example/demoyh/DemoYhApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoYhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoyh/DemoYhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoyh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoYhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
